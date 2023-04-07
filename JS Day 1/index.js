deac01cc03e15b554902fc33de610e6c8876692e
Siddesh Jaiswal <siddeshjaiswal12@gmail.com>
2023-04-07 21:25:23 +0530

    JS Day 1

---
+++ JS Day 1/index.js
@@ -0,0 +1,123 @@
+// alert("hello Guys")
+//variable Its a memory location inside a memory.
+// let age=18;
+// var country='india';
+// let name="Siddesh";
+// const Username="Sid";
+
+// let age=20;
+// age=21;
+// console.log(age);
+
+// Var Redeclare
+// Let Redeclare is not possib
+
+// console.log(age);
+// let age=20;
+
+//String , Number , Boolean
+
+// let name="high";
+// console.log(typeof (name)); //String
+// let type=Boolean(name);
+// console.log(typeof type);
+
+// let age=20;
+// console.log(typeof age);
+// let type=String(age);
+// console.log(typeof type);
+
+// let adult=true;
+// console.log(typeof adult);
+// let type=Number(adult);
+// console.log(typeof type);
+
+
+// let age=20;
+// console.log(typeof (age));
+// let isAdult=true;
+// console.log(typeof (isAdult));
+
+// Conditional Statement
+// let age=17;
+// if(age>18)
+// {
+//     console.log("Person can vote!!!")
+// }
+// else
+// {
+//     console.log("Person can't vote!!!");
+// }
+
+// if(condition)
+// {
+
+// }
+
+// let num1=10;
+// let num2=10;
+
+// Arithmetic Operator + , - , * , / , % , **
+// console.log(num1+num2); //30
+// console.log(num1-num2); //-10
+// console.log(num1*num2); //200
+// console.log(10/2); //5
+// console.log(10%2); // 0
+// console.log(num1**3); // 1000
+
+// Realtional Opeartor > , < , >= , <= , ==
+// console.log(num1>num2); //false
+// console.log(num1<num2); //false
+// console.log(num1>=num2); //true
+// console.log(num1<=num2); //true 
+// console.log(num1==num2); //true
+
+
+// Assignment Operator = , +=, -=, /= , *=
+
+// let num1=10;
+
+// num1+=20; // num1=num1+20
+// console.log(num1); //30
+
+// num1-=20; // num1=num1-20
+// console.log(num1); //10
+
+// num1*=20; // num1=num1*20
+// console.log(num1); //200
+
+// num1/=20; // num1=num1/20
+// console.log(num1);  //10
+
+// Increment / Decrement ++ , --
+// let a=20;
+// a--;
+// console.log(a);
+
+//Logical Operator && , || , !
+// console.log(5>6 && 16>10) //false && false
+// T T = T
+// F T = F
+// T F = F
+// F F = F
+
+// console.log(5>6 || 6>10)
+// // T T = T
+// // F T = T
+// // T F = T
+// // F F = F
+
+// console.log(!(15>6));
+
+// T --> F
+// F --> T
+
+// Conditional Operator 
+// condition ? Print1 : print2;
+let age=10;
+(age>18) ? console.log("Person can vote") : console.log("Person can't vote")
+
+
+
+
+
