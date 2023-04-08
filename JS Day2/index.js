70d86ce1ec2ab615087d4ba40a33aaf9432ceeca
Siddesh Jaiswal <siddeshjaiswal12@gmail.com>
2023-04-08 21:34:25 +0530

    JS Day 2

---
+++ JS Day2/index.js
@@ -0,0 +1,158 @@
+// console.log("I'm attached!!!")
+// Else If ladder
+// let num1=10;
+// let num2=20;
+// if(num1>num2)
+// {
+//     console.log(num1+" is Greater");
+// }
+// else if(num1==num2)
+// {
+//     console.log("Both are equal");
+// }
+// else
+// {
+//     console.log(num2+" is Greater");
+// }
+
+// While Loop
+// Initialisation
+// while(condition)
+// {
+//     // incre/Decre
+// }
+
+// let counter=1;
+// while(counter<=5)
+// {
+//     console.log("Hello World!!!");
+//     counter++;
+// }
+// console.log("While loop ended!!!");
+
+// Initialisation
+// do{
+    // Inc/Decr
+// }
+// while(condition);
+
+// let i=1;
+// do{
+//     console.log("I'm Working");
+//     i++;
+// }
+// while(i<=10);
+// console.log("Do-While loop ended!!!");
+
+// For Loop
+// for(Initialisation ; Condition; Incr/Decr )
+// {
+
+// }
+
+//Jump --> Break ; Continue
+// Break --> Stop execution of Whole loop
+// Continue --> Skip current Iteration
+// for(let i=1;i<=10;i++)
+// {
+//     if(i==5)
+//     {
+//         continue;
+//     }
+    // console.log(i);
+// }
+// console.log("Our loop ended");
+// let num1=20;
+// let num2=10;
+// let sum=num1+num2;
+// console.log(sum);
+
+// num1=50;
+// num2=100;
+// sum=num1+num2;
+// console.log(sum);
+
+// Function 1.UserDefine -- Normal / Anonymous / Arrow  2.Predefine
+
+// FUnction 1.Define the body of Function
+// 2.call that fuction
+
+// function func_Name()
+// {
+
+// }
+
+// func_Name(); //FUnction Call
+
+// function Addition(num1,num2)
+// {
+//     let sum=num1+num2;
+//     console.log("The addition of number is : "+sum)
+// }
+
+// Addition(10,20); //30
+// Addition(20,30); //50
+// Addition(50,100); //150
+
+//Anonymous Function
+// let show=function()
+// {
+//     console.log("Hello");
+// }
+
+// show();
+
+// Arrow Function
+// func_Name=(a,b)=>
+// {
+//     console.log(a+b);
+// }
+
+// func_Name(2,3)
+
+// sum=(a,b)=>
+// {
+//     console.log(a+b);
+// }
+
+// sum(2,3);
+
+//Argument --> When function is called the value that is passed we called that value as Argument
+
+// Predefined Function Example
+// Math Function
+// let x=4.9;
+// console.log(x);
+// let roundOff=Math.round(x);
+
+
+// let roundOff=Math.ceil(x);
+// console.log(roundOff);
+
+
+// let roundOff=Math.floor(x);
+// console.log(roundOff);
+
+// let power=Math.pow(10,5);
+// console.log(power);
+
+// let a=Math.ceil(Math.random()*100); //0-1
+// console.log(a); //1-10
+
+// let x=Math.sqrt(100);
+// console.log(x);
+
+// let x=Math.max(-2,4,6,1,0);
+// console.log(x);
+
+
+
+
+
+
+
+
+
+
+
+
